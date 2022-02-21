a98f4d4576c760df6da1d0b4e20db8ab1d7153e2
Jordan Ulmer <96499164+BreadleyCooper@users.noreply.github.com>
2022-02-21 14:56:08 +0000

    Added event listener to change color but not working

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ for (let i = 0;i<256;i++){
     container.appendChild(grid);
     grid.classList.add("grid");
 }
+let squares = document.querySelector(".grid");
+squares.addEventListener(("mouseenter"), () => {
+  event.target.style.color = "grey";
+})
+
+
