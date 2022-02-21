14c89efe3edbdabc9d49b4563edbaf37436ded47
Jordan Ulmer <96499164+BreadleyCooper@users.noreply.github.com>
2022-02-21 15:59:26 +0000

    Still not working

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ for (let i = 0;i<256;i++){
     const grid = document.createElement("div");
     container.appendChild(grid);
     grid.classList.add("grid");
+    
 }
-let squares = document.querySelector(".grid");
-squares.addEventListener(("mouseenter"), () => {
-  event.target.style.color = "grey";
+let squares = container.querySelectorAll(".grid");
+   squares.forEach((grid) => {
+   squares.addEventListener(("mouseenter"), () => {
+   squares.style.color = "grey";
+   console.log("mouseenter")
+}) 
+
 })
 
 
